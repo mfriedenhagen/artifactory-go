cc5e9c214232ec8b51bba6c0fc0f60143336d741
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-02-15 21:39:34 +0100

    Create form and modify elements.

--- src/main/java/com/github/mfriedenhagen/artifactorygo/HomePage.java
+++ src/main/java/com/github/mfriedenhagen/artifactorygo/HomePage.java
@@ -1,8 +1,10 @@
 package com.github.mfriedenhagen.artifactorygo;
 
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.TextField;
 
 public class HomePage extends WebPage {
 
@@ -10,10 +12,25 @@ public class HomePage extends WebPage {
 
     public HomePage(final PageParameters parameters) {
         super(parameters);
+        add(new TextFieldWithValue("groupId", "myGroupId"));
+        add(new TextFieldWithValue("artifactId", "myArtifactId"));
+        add(new TextFieldWithValue("version", "myVersion"));
+        add(new Label("wicketVersion", getApplication().getFrameworkSettings().getVersion()));
+    }
 
-        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+    private static class TextFieldWithValue extends TextField {
 
-        // TODO Add your page's components here
+        private final String myvalue;
 
+        public TextFieldWithValue(String textFieldId, String myvalue) {
+            super(textFieldId);
+            this.myvalue = myvalue;
+        }
+
+        @Override
+        protected void onComponentTag(ComponentTag tag) {
+            super.onComponentTag(tag);
+            tag.put("value", myvalue);
+        }
     }
 }
